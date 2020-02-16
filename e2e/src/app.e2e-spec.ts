79c02655240c58af9960b57d00bb3fe4f7e33d36
mitsuoysharag <mitsuoysharag@gmail.com>
2020-02-15 23:25:29 -0500

    deploy project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('beinplace-lite app is running!');
+    expect(page.getTitleText()).toEqual('beinplace app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/beinplace-lite'),
+      dir: require('path').join(__dirname, './coverage/beinplace'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
